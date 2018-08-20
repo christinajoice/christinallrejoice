9ea0b9803d2a35968088b05c0743508f88dcb39a
christinajoice <christinallrejoice@gmail.com>
2018-08-20 14:00:52 +0530

    Update and rename Leap.java to Leapyear.java

---
+++ Leapyear.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+class Leapyear
+{
+public static void main(String args[])
+{
+System.out.println("Enter a year");
+Scanner s=new Scanner(System.in);
+int year=s.nextInt();
+if(year/4==0)
+{
+if(year/100==0)
+{
+if(year/400==0)
+{
+System.out.println("Leap year");
+}
+}
+}
+else
+{
+System.out.println("not a leap year");
+}
+}
+}
