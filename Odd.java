dd8a3e7414d22fee940573bf90cd3258335e53c1
christinajoice <christinallrejoice@gmail.com>
2018-08-27 13:52:37 +0530

    Create Odd.java

---
+++ Odd.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+class Odd
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number intervals");
+int a=s.nextInt();
+int b=s.nextInt();
+for(int i=a;i<=b;i++)
+{
+if(i%2!=0)
+{
+System.out.println(i);
+}
+}
+}
+}
